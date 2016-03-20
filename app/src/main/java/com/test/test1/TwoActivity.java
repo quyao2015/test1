3d26ca3eb4e7959ed6bdf6dd8f153dfc89261bb3
quyao <8197740@qq.com>
2016-03-20 15:59:40 +0800

    ONE2

--- app/src/main/java/com/test/test1/TwoActivity.java
+++ app/src/main/java/com/test/test1/TwoActivity.java
@@ -2,32 +2,31 @@ package com.test.test1;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class TwoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_main2);
+
+        setContentView(R.layout.activity_main3);
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "你惦记我", Toast.LENGTH_SHORT).show();
+                Toast.makeText(TwoActivity.this, "你惦记我", Toast.LENGTH_SHORT).show();
             }
         });
         Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,TwoActivity.class);
-                startActivity(intent);
+                finish();
 
             }
         });
